c13f19280365c7f0305e86fc9fc7c9695f0fb0c9
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-06-16 13:48:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (52.10%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long reverseNumber = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+            reverseNumber = reverseNumber * 10 + digit;
+            if (reverseNumber > INT_MAX || reverseNumber < INT_MIN) {
+                return 0;
+            }
+        }
+        return static_cast<int>(reverseNumber); 
+    }
+};
