dff1e50a33622a2b00cf3c628d3b903211467dd1
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-06-21 22:53:30 +0530

    Time: 18 ms (6.26%) | Memory: 12.9 MB (54.38%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        sort(nums2.begin(),nums2.end());
         vector<int> visited(n,-1);
         set<int> temp;
         int i=0;
@@ -12,10 +11,6 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(nums1[i]<nums2[j])
-                {
-                    break;
-                }
                 if(visited[i] == -1  && nums1[i]==nums2[j])
                 {
                     visited[i]=1;
