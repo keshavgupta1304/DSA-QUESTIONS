82acb2407097118cf5db568eb814fc5c28dd522e
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-06-30 00:19:39 +0530

    Time: 14 ms (86.91%) | Memory: 12.2 MB (63.61%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res="";
+        for(char&c :s)
+        {
+            if(res.size() && c==res.back())
+            {
+                res.pop_back();
+            }
+            else
+            {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
