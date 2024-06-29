9c2ecbb9fa4d8724b9c31a649cdfb48bf5c0e04e
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-06-29 14:00:28 +0530

    Time: 11 ms (39.34%) | Memory: 10.4 MB (24.30%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     bool checkInclusion(std::string s1, std::string s2) {
-        if (s2.size() < s1.size()) {
+        if(s2.size()<s1.size())
+        {
             return false;
         }
-
-        int frequency[26] = {0};
-
-        for (auto ch : s1) {
-            frequency[ch - 'a']++;
+        unordered_map<char,int> freq_s1;
+        unordered_map<char,int> freq_s2;
+        for(int i=0;i<s1.length();i++)
+        {
+            freq_s1[s1[i]]++;
+            freq_s2[s2[i]]++;
         }
 
-        int start = 0;
-        int current_frequency[26];
-        for (int end = 0; end < s2.size(); end++) {
-            int end_char = s2[end] - 'a';
-            current_frequency[end_char]++;
+        if(freq_s1 == freq_s2)
+        {
+            return true;
+        }
 
-            // We have to move the sliding window, until we remove
-            // the first occurence of the character at the end of 
-            // the sliding window, so that the 
-            // current_frequency[end_char] == frequency[end_char]
-            while (current_frequency[end_char] > frequency[end_char]) {
-                current_frequency[s2[start] - 'a']--;
-                start++;
-            } 
+        for(int i=s1.length();i<s2.length();i++)
+        {
+            char ch_out=s2[i-s1.length()];
+            if(freq_s2[ch_out] == 1)
+            {
+                freq_s2.erase(ch_out);
+            }
+            else
+            {
+                freq_s2[ch_out]--;
+            }
 
-            // if the length of the sliding window is equal to s1.size()
-            // it means that it is a permutation of s1
-            if (end - start + 1 == s1.size()) {
+            char ch_in=s2[i];
+            freq_s2[ch_in]++;
+
+            if(freq_s1 == freq_s2)
+            {
                 return true;
             }
 
         }
-
         return false;
     }
 };
