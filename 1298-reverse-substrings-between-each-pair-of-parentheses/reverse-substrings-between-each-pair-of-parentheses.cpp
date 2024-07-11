d099b79691eaadf5b568a3ac3f753f0b67f211de
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-07-11 15:07:52 +0530

    Time: 3 ms (49.64%) | Memory: 7.8 MB (52.67%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<char> stk;
-        
-        for(int i = 0; i < s.length(); i++) {
-            if (s[i] == ')') {
-                string reverse = "";
-                // Pop from the stack until an opening parenthesis is found
-                while (!stk.empty() && stk.top() != '(') {
-                    reverse += stk.top();
-                    stk.pop();
-                }
-                // Pop the opening parenthesis from the stack
-                if (!stk.empty()) {
-                    stk.pop();
-                }
-                // Push the reversed string back onto the stack
-                for (char ch : reverse) {
-                    stk.push(ch);
-                }
-            } else {
-                stk.push(s[i]);
+        int n=s.length();
+        stack<int> opened;
+        vector<int> matching(n);
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                opened.push(i);
+            }
+            if(s[i]==')')
+            {
+                int j = opened.top();
+                opened.pop();
+                matching[i]=j;
+                matching[j]=i;
             }
         }
-        
-        // Form the result string from the stack
-        string ans = "";
-        while (!stk.empty()) {
-            ans = stk.top() + ans;
-            stk.pop();
+        string result;
+        for(int index=0,direction=1;index<n;index=index+direction)
+        {
+            if(s[index]=='(' || s[index]==')')
+            {
+                index=matching[index];
+                direction=-direction;
+            }
+            else
+            {
+                result+=s[index];
+            }
         }
-        return ans;
+        return result;
+
     }
 };
