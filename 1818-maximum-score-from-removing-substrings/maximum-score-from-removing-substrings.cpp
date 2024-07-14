4e85e3800b2aa9137f436ffd4e33f7614feebffb
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-07-14 12:58:32 +0530

    Time: 72 ms (66.83%) | Memory: 26.1 MB (40.90%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int res = 0;
-        string top, bot;
-        int top_score, bot_score;
-
-        if (y > x) {
-            top = "ba";
-            top_score = y;
-            bot = "ab";
-            bot_score = x;
-        } else {
-            top = "ab";
-            top_score = x;
-            bot = "ba";
-            bot_score = y;
+        if (s.length() < 2) {
+            return 0; 
         }
 
-        // Removing first top substrings cause they give more points
-        vector<char> stack;
-        for (char ch : s) {  // Changed 'char' to 'ch'
-            if (ch == top[1] && !stack.empty() && stack.back() == top[0]) {
-                res += top_score;
-                stack.pop_back();
-            } else {
-                stack.push_back(ch);
+        int ans = 0;
+        string priorityPair = (x > y) ? "ab" : "ba";
+        string secondaryPair = (x > y) ? "ba" : "ab";
+        int priorityValue = max(x, y);
+        int secondaryValue = min(x, y);
+
+        // Function to remove pairs and calculate the gain
+        auto removePairs = [&](const string& pair, int value) {
+            string result;
+            int gain = 0;
+            for (char c : s) {
+                if (!result.empty() && result.back() == pair[0] && c == pair[1]) {
+                    result.pop_back();
+                    gain += value;
+                } else {
+                    result.push_back(c);
+                }
             }
-        }
+            s = result;
+            return gain;
+        };
 
-        // Removing bot substrings cause they give less or equal amount of scores
-        vector<char> new_stack;
-        for (char ch : stack) {  // Changed 'char' to 'ch'
-            if (ch == bot[1] && !new_stack.empty() && new_stack.back() == bot[0]) {
-                res += bot_score;
-                new_stack.pop_back();
-            } else {
-                new_stack.push_back(ch);
-            }
-        }
+        // First remove priority pairs, then secondary pairs
+        ans += removePairs(priorityPair, priorityValue);
+        ans += removePairs(secondaryPair, secondaryValue);
 
-        return res;
+        return ans;
     }
 };
