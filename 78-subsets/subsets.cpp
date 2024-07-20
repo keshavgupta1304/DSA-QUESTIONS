402a3c09b007f607c1a462b0639640d8456b2294
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-07-20 23:46:05 +0530

    Time: 5 ms (13.18%) | Memory: 8.8 MB (39.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> output;
-        output.push_back({});
-        for (int num : nums) {
-            vector<vector<int>> newSubsets;
-            for (vector<int> curr : output) {
-                vector<int> temp = curr;
-                temp.push_back(num);
-                newSubsets.push_back(temp);
+        for (int i = pow(2, n); i < pow(2, n + 1); ++i) {
+            // generate bitmask, from 0..00 to 1..11
+            string bitmask = bitset<32>(i).to_string().substr(32 - n);
+            // append subset corresponding to that bitmask
+            vector<int> curr;
+            for (int j = 0; j < n; ++j) {
+                if (bitmask.at(j) == '1') curr.push_back(nums[j]);
             }
-            for (vector<int> curr : newSubsets) {
-                output.push_back(curr);
-            }
+            output.push_back(curr);
         }
         return output;
     }
