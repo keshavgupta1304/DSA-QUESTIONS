1415c63f7980a3c45800be76fcd4e0bcee6a8c90
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-07-23 22:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (46.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,7 +4,7 @@ public:
         int arr[31];
         arr[0]=0;
         arr[1]=1;
-        for(int i=2;i<31;i++)
+        for(int i=2;i<=n;i++)
         {
             arr[i]=arr[i-1]+arr[i-2];
         }
