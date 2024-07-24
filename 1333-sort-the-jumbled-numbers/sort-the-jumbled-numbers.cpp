e4314a8c54e9d8138ed94272e901baf570bdd6a2
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-07-24 23:47:06 +0530

    Time: 126 ms (97.40%) | Memory: 87.8 MB (64.94%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -4,16 +4,21 @@ public:
         vector<pair<int, int>> storePairs;
 
         for (int i = 0; i < nums.size(); ++i) {
-            // Convert current value to string
-            string number = to_string(nums[i]);
-            string formed = "";
-            for (int j = 0; j < number.size(); ++j) {
-                formed = formed + (to_string(mapping[number[j] - '0']));
+            int mappedValue = 0;
+            int temp = nums[i];
+            // Start making changes from the units place.
+            int place = 1;
+            // If the value initially is 0, return mapping[0] and index.
+            if (temp == 0) {
+                storePairs.push_back({mapping[0], i});
+                continue;
             }
-            // Store the mapped value.
-            int mappedValue = stoi(formed);
-            // Push a pair consisting of mapped value and original value;s
-            // index.
+            // Repeat the process for units, tenths, hundredths.. places.
+            while (temp != 0) {
+                mappedValue = place * mapping[temp % 10] + mappedValue;
+                place *= 10;
+                temp /= 10;
+            }
             storePairs.push_back({mappedValue, i});
         }
 
