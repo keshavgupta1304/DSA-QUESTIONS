83a2b7324a83a3dbc6cc9854d779bb7773aa5e9f
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-08-01 21:13:17 +0530

    Time: 9 ms (23.77%) | Memory: 17.7 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans=0;
+        for(auto detail:details)
+        {
+            string s=detail.substr(11,2);
+            if(stoi(s)>60)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
