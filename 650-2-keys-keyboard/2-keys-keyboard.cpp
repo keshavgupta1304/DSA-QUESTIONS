016c3d8f7acfed92c41a1537f2dababd4d072e0f
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-08-19 13:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (71.95%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n==1) return 0;
-        return 1 + solve(1,1,n);
-    }
-    int solve(int currentA,int prevCopy,int n)
-    {
-        if(currentA==n) return 0;
-        if(currentA > n) return 1000;
-        int firstChoice=2+solve(currentA*2,currentA,n);
-        int secondChoice=1+solve(currentA+prevCopy,prevCopy,n);
-        return min(firstChoice,secondChoice);
+        int minSteps=0;
+        int primeFactor=2;//initial prime factor
+
+        while(n>1)
+        {
+            while(n%primeFactor==0)
+            {
+                minSteps+=primeFactor;
+                n/=primeFactor;
+            }
+            primeFactor++;
+        }
+        return minSteps;
     }
 };
