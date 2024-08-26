f0796664c16390c5c7faa6693f977ab3a9ad10be
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-08-27 01:26:55 +0530

    Time: 9 ms (57.09%) | Memory: 18.8 MB (45.65%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int occurencesRequired;
-        if(n<3)
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
+        for(int i=0;i<nums.size();i++)
         {
-            occurencesRequired = 1;
-        }
-        else
-        {
-            occurencesRequired = n/3 + 1;
-        }
-        
-        unordered_map<int,int> occurences;
-        for(auto num:nums)
-        {
-            occurences[num]++;
+            if(cnt1==0 && el2!=nums[i])
+            {
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && el1!=nums[i])
+            {
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
         vector<int> ans;
-        for(auto val:occurences)
+        cnt1=0,cnt2=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(val.second>=occurencesRequired) ans.push_back(val.first);
+            if(nums[i]==el1) cnt1++;
+            else if(nums[i]==el2) cnt2++;
         }
+        int requiredOccurrences = ((nums.size())/3) + 1;
+        if(cnt1>=requiredOccurrences) ans.push_back(el1);
+        if(cnt2>=requiredOccurrences) ans.push_back(el2);
         return ans;
     }
 };
