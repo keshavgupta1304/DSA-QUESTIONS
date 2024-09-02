e854aca9badcdc0a0117ee54f7932fb644ee52bc
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2024-09-03 00:41:01 +0530

    Time: 5 ms (52.03%) | Memory: 16.8 MB (26.38%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            {
                 return true;
             }
-
-            // If we have duplicates, we can skip them
-            if (nums[left] == nums[mid] && nums[right] == nums[mid]) {
-                left++;
-                right--;
-            } 
-            // If left part is sorted
-            else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            if(nums[mid]==nums[left] && nums[mid]==nums[right])
+            {
+                left=left+1;
+                right=right-1;
+                continue;
+            }
+            else if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target && target<=nums[mid])
+                {
+                    right=mid-1;
                 }
-            } 
-            // If right part is sorted
-            else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                else{
+                    left=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target && target<=nums[right])
+                {
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
                 }
             }
         }
-
         return false;
     }
 };
