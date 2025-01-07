7f4bab43997fcb2c4cbf8b5a3f400676c6a3e516
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-01-07 21:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (76.01%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string> &words) {
+        vector<string> matchingWords;
+        for (int currentWordIndex = 0; currentWordIndex < words.size();
+             currentWordIndex++) {
+            for (int otherWordIndex = 0; otherWordIndex < words.size();
+                 otherWordIndex++) {
+                if (currentWordIndex == otherWordIndex)
+                    continue; 
+                if (isSubstringOf(words[currentWordIndex],
+                                  words[otherWordIndex])) {
+                    matchingWords.push_back(words[currentWordIndex]);
+                    break; 
+                }
+            }
+        }
+        return matchingWords;
+    }
+
+private:
+    bool isSubstringOf(string &sub, string &main) {
+        for (int startIndex = 0; startIndex < main.size(); startIndex++) {
+            bool subFits = true;
+            for (int subIndex = 0; subIndex < sub.size(); subIndex++) {
+                if (startIndex + subIndex >= main.size() ||
+                    main[startIndex + subIndex] != sub[subIndex]) {
+                    subFits = false;
+                    break; 
+                }
+            }
+            if (subFits) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+};
