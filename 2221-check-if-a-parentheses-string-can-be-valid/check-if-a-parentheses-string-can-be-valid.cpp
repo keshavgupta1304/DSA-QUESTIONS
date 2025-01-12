e8f37446d25b1fe1d761b1e0d180677db9cab7bf
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-01-12 21:28:14 +0530

    Time: 12 ms (68.27%) | Memory: 34.9 MB (8.88%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n=locked.size();
+        if(n%2==1) return false;
+        stack<int> openBrackets,unlocked;
+        for(int i=0;i<n;i++)
+        {
+            if(locked[i]=='0') unlocked.push(i);
+            else if(s[i]=='(') openBrackets.push(i);
+            else if(s[i]==')')
+            {
+                if(!openBrackets.empty()) openBrackets.pop();
+                else if(!unlocked.empty()) unlocked.pop();
+                else return false;
+            }
+        }
+        while(!unlocked.empty() && !openBrackets.empty() && openBrackets.top()<unlocked.top())
+        {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+        if(!openBrackets.empty()) return false;
+        return true;
+    }
+};
