be42e7413eb10bd5b992dbd19048619129eed395
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-02-18 18:34:18 +0530

    Time: 3 ms (33.98%) | Memory: 15.3 MB (25.44%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,24 @@
 class Solution {
+private:
+    void recursion(vector<vector<int>>& ans,vector<int>& subset,int index,vector<int> nums)
+    {
+        if(index>=nums.size())
+        {
+            ans.push_back(subset);
+            return;
+        }
+        //include
+        subset.push_back(nums[index]);
+        recursion(ans,subset,index+1,nums);
+        subset.pop_back();
+        //exclude
+        recursion(ans,subset,index+1,nums);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> output;
-        for (int i = pow(2, n); i < pow(2, n + 1); ++i) {
-            // generate bitmask, from 0..00 to 1..11
-            string bitmask = bitset<32>(i).to_string().substr(32 - n);
-            // append subset corresponding to that bitmask
-            vector<int> curr;
-            for (int j = 0; j < n; ++j) {
-                if (bitmask.at(j) == '1') curr.push_back(nums[j]);
-            }
-            output.push_back(curr);
-        }
-        return output;
+        vector<vector<int>> ans;
+        vector<int> subset;
+        recursion(ans,subset,0,nums);
+        return ans;
     }
 };
