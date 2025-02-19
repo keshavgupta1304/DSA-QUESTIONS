4b2c6ea81d1932a9470ea36cc11c056ab2e6172a
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-02-20 02:19:26 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (80.75%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int totalWater=0;
-        int n=height.size();
-        vector<int> prefixMax(n);
-        prefixMax[0]=height[0];
-        for(int i=1;i<n;i++)
-        {
-            prefixMax[i]=max(prefixMax[i-1],height[i]);
-        }
-        vector<int> suffixMax(n);
-        suffixMax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            suffixMax[i]=max(suffixMax[i+1],height[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            int leftMax=prefixMax[i],rightMax=suffixMax[i];
-            if(height[i]<leftMax && height[i]<rightMax)
-            {
-                totalWater+=min(leftMax,rightMax)-height[i];
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
             }
         }
+
         return totalWater;
     }
 };
