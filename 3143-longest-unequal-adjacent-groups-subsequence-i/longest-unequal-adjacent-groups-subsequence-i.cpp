e3481e9811a2fdfb11f2bd8eabedfc6f48f6aec7
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-05-15 19:14:49 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (71.98%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    bool generateIndexes(const vector<int>& groups,int groupNo,int index,vector<int>& indexes)
-    {
-        if(index==groups.size())
-        {
-            return true;
-        }
-        //pick the element
-        if(groups[index]!=groupNo)
-        {
-            indexes.push_back(index);
-            if(generateIndexes(groups,groups[index],index+1,indexes)) return true;
-            indexes.pop_back();
-        }
-        //do not pick the element
-        if(generateIndexes(groups,groupNo,index+1,indexes)) return true;
-        return false;
-    }
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        vector<int> indexes;
-        generateIndexes(groups,-1,0,indexes);
-        vector<string> result;
-        for(auto index:indexes)
-        {
-            result.push_back(words[index]);
+        vector<string> ans;
+        int n = words.size();
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.emplace_back(words[i]);
+            }
         }
-        return result;
+        return ans;
     }
 };
