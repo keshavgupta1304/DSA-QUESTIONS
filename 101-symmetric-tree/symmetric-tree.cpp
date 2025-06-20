3639c03288490b0403b499d582a3134eea534a48
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-06-20 23:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (61.18%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,64 +11,13 @@
  */
 class Solution {
 public:
-    int depth(TreeNode* root)
-    {
-        if(!root) return 0;
-        int lh=depth(root->left);
-        int rh=depth(root->right);
-        return 1+max(lh,rh);
-    }
     bool isSymmetric(TreeNode* root) {
-        if(!root) return true;
-        queue<TreeNode*> q;
-        q.push(root);
-        int dep=depth(root);
-        int l=0;
-        vector<int> level;
-        while(!q.empty())
-        {
-            level={};
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                TreeNode* node=q.front();
-                q.pop();
-                // if(node->left==nullptr && node->right==nullptr) continue;
-                if(node->left!=nullptr && node->right==nullptr)
-                {
-                    q.push(node->left);
-                    TreeNode* newNode=new TreeNode(-101);
-                    q.push(newNode);
-                }
-                else if(node->left==nullptr && node->right!=nullptr)
-                {
-                    TreeNode* newNode=new TreeNode(-101);
-                    q.push(newNode);
-                    q.push(node->right);
-                }
-                else if(node->left!=nullptr && node->right!=nullptr)
-                {
-                    q.push(node->left);
-                    q.push(node->right);
-                }
-                else
-                {
-                    if(l+1<dep)
-                    {
-                        TreeNode* newNode1=new TreeNode(-101);
-                        TreeNode* newNode2=new TreeNode(-101);
-                        q.push(newNode1);
-                        q.push(newNode2);
-                    }
-                }
-                level.push_back(node->val);
-            }
-            l++;
-            for(int i=0;i<level.size()/2;i++)
-            {
-                if(level[i]!=level[level.size()-i-1]) return false;
-            }
-        }
-        return true;
+        return root==NULL || helper(root->left,root->right);
+    }
+    bool helper(TreeNode* left,TreeNode* right)
+    {
+        if(!left || !right) return left==right;
+        if(left->val!=right->val) return false;
+        return helper(left->left,right->right) && helper(left->right,right->left);
     }
 };
