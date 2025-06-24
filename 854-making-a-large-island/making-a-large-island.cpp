ea9b9ed854c83551ea200e0d116854495e1c5e80
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-06-24 23:44:41 +0530

    Time: 286 ms (41.19%) | Memory: 159.5 MB (32.45%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,79 +1,83 @@
-class Solution {
-private:
-    int exploreIsland(vector<vector<int>>& grid, int islandId, int currentRow,
-                      int currentColumn) {
-        if (currentRow < 0 || currentRow >= grid.size() || currentColumn < 0 ||
-            currentColumn >= grid[0].size() ||
-            grid[currentRow][currentColumn] != 1)
-            return 0;
-
-        grid[currentRow][currentColumn] = islandId;
-        return 1 +
-               exploreIsland(grid, islandId, currentRow + 1, currentColumn) +
-               exploreIsland(grid, islandId, currentRow - 1, currentColumn) +
-               exploreIsland(grid, islandId, currentRow, currentColumn + 1) +
-               exploreIsland(grid, islandId, currentRow, currentColumn - 1);
+class DSU{
+    public:
+    vector<int> parent,size;
+    DSU(int n)
+    {
+        parent=vector<int>(n+1);
+        size=vector<int>(n+1,1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+        }
     }
+    int findUPar(int u){
+        if(u==parent[u]) return u;
+        return parent[u]=findUPar(parent[u]);
+    }
+    void unionBySize(int u,int v)
+    {
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
 
+class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
-        unordered_map<int, int> islandSizes;
-        int islandId = 2;
-        for (int currentRow = 0; currentRow < grid.size(); ++currentRow) {
-            for (int currentColumn = 0; currentColumn < grid[0].size();
-                 ++currentColumn) {
-                if (grid[currentRow][currentColumn] == 1) {
-                    islandSizes[islandId] = exploreIsland(
-                        grid, islandId, currentRow, currentColumn);
-                    ++islandId;
+        int n=grid.size();
+        int m=grid[0].size();
+        DSU ds(n*m);
+        vector<pair<int,int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
+        for(int row=0;row<n;row++)
+        {
+            for(int col=0;col<m;col++)
+            {
+                if(grid[row][col]==0) continue;
+                for(auto [dx,dy]:dir)
+                {
+                    int newRow=row+dx;
+                    int newCol=col+dy;
+                    if(newRow<0 || newRow>=n || newCol<0 || newCol>=m || !grid[newRow][newCol]) continue;
+                    int node=row*m+col;
+                    int newNode=newRow*m+newCol;
+                    ds.unionBySize(node,newNode);
                 }
             }
         }
-        if (islandSizes.empty()) {
-            return 1;
-        }
-        if (islandSizes.size() == 1) {
-            --islandId;
-            return (islandSizes[islandId] == grid.size() * grid[0].size())
-                       ? islandSizes[islandId]
-                       : islandSizes[islandId] + 1;
-        }
-        int maxIslandSize = 1;
-        for (int currentRow = 0; currentRow < grid.size(); ++currentRow) {
-            for (int currentColumn = 0; currentColumn < grid[0].size();
-                 ++currentColumn) {
-                if (grid[currentRow][currentColumn] == 0) {
-                    int currentIslandSize = 1;
-                    unordered_set<int> neighboringIslands;
-                    if (currentRow + 1 < grid.size() &&
-                        grid[currentRow + 1][currentColumn] > 1) {
-                        neighboringIslands.insert(
-                            grid[currentRow + 1][currentColumn]);
-                    }
-                    if (currentRow - 1 >= 0 &&
-                        grid[currentRow - 1][currentColumn] > 1) {
-                        neighboringIslands.insert(
-                            grid[currentRow - 1][currentColumn]);
-                    }
-                    if (currentColumn + 1 < grid[0].size() &&
-                        grid[currentRow][currentColumn + 1] > 1) {
-                        neighboringIslands.insert(
-                            grid[currentRow][currentColumn + 1]);
-                    }
-                    if (currentColumn - 1 >= 0 &&
-                        grid[currentRow][currentColumn - 1] > 1) {
-                        neighboringIslands.insert(
-                            grid[currentRow][currentColumn - 1]);
-                    }
-                    for (int id : neighboringIslands) {
-                        currentIslandSize += islandSizes[id];
-                    }
-
-                    maxIslandSize = max(maxIslandSize, currentIslandSize);
+        int maxi=INT_MIN;
+        for(int row=0;row<n;row++)
+        {
+            for(int col=0;col<m;col++)
+            {
+                if(grid[row][col]) continue;
+                unordered_set<int> components;
+                for(auto [dx,dy]:dir)
+                {
+                    int newRow=row+dx;
+                    int newCol=col+dy;
+                    if(newRow<0 || newRow>=n || newCol<0 || newCol>=m || !grid[newRow][newCol]) continue;
+                    int newNode=newRow*m+newCol;
+                    components.insert(ds.findUPar(newNode));
                 }
+                int islandSize=1;
+                for(auto num:components)
+                {
+                    islandSize+=ds.size[num];
+                }
+                maxi=max(maxi,islandSize);
             }
         }
-
-        return maxIslandSize;
+        return maxi==INT_MIN?n*m:maxi;
     }
 };
