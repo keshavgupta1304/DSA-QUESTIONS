1ca5ce0cef9d22c6069e7bc975f63107de8882c3
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-05 17:36:32 +0530

    Time: 3 ms (32.62%) | Memory: 15 MB (38.84%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
-    int recur(int i,int j,const vector<vector<int>>& matrix,vector<vector<int>>& dp)
-    {
-        if(j<0 || j>=matrix.size()) return INT_MAX;
-        if(i==matrix.size()-1) return matrix[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int belowLeft=recur(i+1,j-1,matrix,dp);
-        int belowRight=recur(i+1,j+1,matrix,dp);
-        int below=recur(i+1,j,matrix,dp);
-        return dp[i][j]=matrix[i][j]+min({belowLeft,belowRight,below});
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        int mini=INT_MAX;
         vector<int> prev(n);
         for(int i=0;i<n;i++)
         {
@@ -30,10 +19,6 @@ public:
             }
             prev=temp;
         }
-        for(int i=0;i<n;i++)
-        {
-            mini=min(mini,prev[i]);
-        }
-        return mini;
+        return *min_element(prev.begin(),prev.end());
     }
 };
