cfcfeb5fd5ce9d9f13fc01c0e9aefeab3c12a868
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-05 13:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.53%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<int> prev;
-        prev.push_back(triangle[0][0]);
-        for(int i=1;i<triangle.size();i++)
-        {
-            vector<int> temp;
-            for(int j=0;j<=i;j++)
-            {
-                int upLeft=1e5;
-                int up=1e5;
-                if(j-1>=0) upLeft=prev[j-1];
-                if(j<prev.size()) up=prev[j];
-                temp.push_back(min(upLeft,up) + triangle[i][j]);
+        vector<int> prev = triangle[0]; 
+        for(int i = 1; i < triangle.size(); i++) {
+            prev.push_back(0);
+            for(int j = i; j >= 0; j--) {
+                if(j == 0) {
+                    prev[j] = prev[j] + triangle[i][j]; 
+                } else if(j == i) {
+                    prev[j] = prev[j-1] + triangle[i][j]; 
+                } else {
+                    prev[j] = min(prev[j-1], prev[j]) + triangle[i][j];
+                }
             }
-            prev=temp;
         }
-        return *min_element(prev.begin(),prev.end());
+        return *min_element(prev.begin(), prev.end());
     }
 };
