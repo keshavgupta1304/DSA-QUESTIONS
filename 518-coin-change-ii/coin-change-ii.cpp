4d9f11c4fd209403df5b8022c61a6b24e32b4e73
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-07 23:40:48 +0530

    Time: 19 ms (55.28%) | Memory: 28.2 MB (66.40%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<uint> dp(amount+1);
-        dp[0]=1;
-        for(auto coin:coins)
+    int recur(int index,int target,const vector<int>& coins,vector<vector<int>>& dp)
+    {
+        if(index==0)
         {
-            for(int s=coin;s<=amount;s++)
-            {
-                dp[s]+=dp[s-coin];
-            }
+            if(target%coins[index]==0) return 1;
+            else return 0;
         }
-        return dp[amount];
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notTake=recur(index-1,target,coins,dp);
+        int take=0;
+        if(coins[index]<=target)
+        {
+            take=recur(index,target-coins[index],coins,dp);
+        }
+        return dp[index][target]=take+notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return recur(n-1,amount,coins,dp);
     }
 };
