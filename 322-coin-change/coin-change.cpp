a9a6f063b9982a87ee5b8309b25039b6899340af
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-07 22:08:39 +0530

    Time: 51 ms (25.76%) | Memory: 49.4 MB (29.67%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<uint> dp(amount+1,INT_MAX);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++)
+    int recur(int index,int target,const vector<int>& coins,vector<vector<int>>& dp)
+    {
+        if(index==0)
         {
-            for(auto coin:coins)
-            {
-                if(i>=coin) dp[i]=min(dp[i],dp[i-coin]+1);
-            }
+            if(target%coins[0]==0) return target/coins[0];
+            else return 1e9;
         }
-        return dp[amount]==INT_MAX?-1:dp[amount];
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notTake=0+recur(index-1,target,coins,dp);
+        int take=INT_MAX;
+        if(coins[index]<=target)
+        {
+            take=1+recur(index,target-coins[index],coins,dp);
+        }
+        return dp[index][target]=min(notTake,take);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        if(amount==0) return 0;
+        int ans=recur(n-1,amount,coins,dp);
+        return ans>=1e9?-1:ans;
     }
 };
