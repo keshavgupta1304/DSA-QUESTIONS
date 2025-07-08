4f72561c9c842b30bbb5bda7af5b704cc9c1d5fc
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-08 23:53:21 +0530

    Time: 34 ms (38.20%) | Memory: 26.8 MB (72.06%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int recur(int ind1,int ind2,const string& s,const string& t,vector<vector<int>>& dp)
+    {
+        if(ind2<0) return 1;
+        if(ind1<0)
+        {
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        int dontTake=recur(ind1-1,ind2,s,t,dp);
+        int take=0;
+        if(s[ind1]==t[ind2])
+        {
+            take=recur(ind1-1,ind2-1,s,t,dp);
+        }
+        return dp[ind1][ind2]=take+dontTake;
+    }
+    int numDistinct(string s, string t) {
+        int n1=s.size();
+        int n2=t.size();
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        return recur(n1-1,n2-1,s,t,dp);
+    }
+};
