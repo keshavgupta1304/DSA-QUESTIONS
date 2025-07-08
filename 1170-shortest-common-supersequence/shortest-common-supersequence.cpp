12be0bd076bac0a600ac1390695be238a6d8a20e
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-08 23:36:15 +0530

    Time: 23 ms (88.95%) | Memory: 27.1 MB (12.68%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,72 +1,40 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int str1Length = str1.length();
-        int str2Length = str2.length();
-
-        vector<vector<int>> dp(str1Length + 1, vector<int>(str2Length + 1, 0));
-
-        // Initialize the base cases
-        // When str2 is empty, the supersequence is str1 itself
-        // (length = row index)
-        for (int row = 0; row <= str1Length; row++) {
-            dp[row][0] = row;
-        }
-        // When str1 is empty, the supersequence is str2 itself
-        // (length = col index)
-        for (int col = 0; col <= str2Length; col++) {
-            dp[0][col] = col;
-        }
-
-        // Fill the DP table
-        for (int row = 1; row <= str1Length; row++) {
-            for (int col = 1; col <= str2Length; col++) {
-                if (str1[row - 1] == str2[col - 1]) {
-                    // If characters match,
-                    // inherit the length from the diagonal + 1
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
-                } else {
-                    // If characters do not match,
-                    // take the minimum length option + 1
-                    dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + 1;
-                }
+        int n1 = str1.size();
+        int n2 = str2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (str1[i - 1] == str2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-
-        string supersequence = "";
-        int row = str1Length, col = str2Length;
-
-        while (row > 0 && col > 0) {
-            if (str1[row - 1] == str2[col - 1]) {
-                // If characters match, take it from diagonal
-                supersequence += str1[row - 1];
-                row--;
-                col--;
-            } else if (dp[row - 1][col] < dp[row][col - 1]) {
-                // If str1’s character is part of the supersequence, move up
-                supersequence += str1[row - 1];
-                row--;
+        string res(n1 + n2 - dp[n1][n2],'0');
+        int index = n1 + n2 - dp[n1][n2] - 1;
+        int i = n1, j = n2;
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                if (index >= 0)
+                    res[index--] = str1[i - 1];
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                if (index >= 0)
+                    res[index--] = str1[i - 1];
+                i--;
             } else {
-                // If str2’s character is part of the supersequence, move left
-                supersequence += str2[col - 1];
-                col--;
+                if (index >= 0)
+                    res[index--] = str2[j - 1];
+                j--;
             }
         }
-
-        // Append any remaining characters
-        // If there are leftover characters in str1
-        while (row > 0) {
-            supersequence += str1[row - 1];
-            row--;
-        }
-        // If there are leftover characters in str2
-        while (col > 0) {
-            supersequence += str2[col - 1];
-            col--;
-        }
-
-        // Reverse the built sequence to get the correct order
-        reverse(supersequence.begin(), supersequence.end());
-        return supersequence;
+        while (i > 0)
+            res[index--] = str1[i - 1], i--;
+        while (j > 0)
+            res[index--] = str2[j - 1], j--;
+        return res;
     }
 };
