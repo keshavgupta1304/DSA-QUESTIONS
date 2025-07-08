85d581997e0220402604b8bb380a79c5274fa983
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-08 14:21:16 +0530

    Time: 107 ms (88.71%) | Memory: 113.7 MB (83.42%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -15,28 +15,23 @@ public:
         }
         return ans;
     }
-    int recur(int index, int limit, const vector<vector<int>>& events,
-              vector<vector<int>>& dp) {
-        if (limit == 0)
-            return 0;
-        if (index == events.size()) {
-            return 0;
-        }
-        if (dp[index][limit] != -1)
-            return dp[index][limit];
-        int notTake = recur(index + 1, limit, events, dp);
-
-        int nextIndexToTake = bs(events, events[index][1]);
-        int take = events[index][2];
-        if (nextIndexToTake != -1) {
-            take += recur(nextIndexToTake, limit - 1, events, dp);
-        }
-        return dp[index][limit] = max(take, notTake);
-    }
     int maxValue(vector<vector<int>>& events, int k) {
         sort(events.begin(), events.end());
         int n = events.size();
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
-        return recur(0, k, events, dp);
+        vector<int> nextIndex(n);
+        for (int i = 0; i < n; ++i) {
+            nextIndex[i] = bs(events, events[i][1]);
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int index = n - 1; index >= 0; index--) {
+            for (int limit = 1; limit <= k; limit++) {
+                int notTake = dp[index + 1][limit];
+                int take = events[index][2];
+                if (nextIndex[index] != -1)
+                    take += dp[nextIndex[index]][limit - 1];
+                dp[index][limit] = max(take, notTake);
+            }
+        }
+        return dp[0][k];
     }
 };
