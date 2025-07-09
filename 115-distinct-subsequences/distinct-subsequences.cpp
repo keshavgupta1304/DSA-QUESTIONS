4e01d4801a64f53c9c8b0ceb2e7d2fd8de121107
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-09 22:19:47 +0530

    Time: 25 ms (73.54%) | Memory: 27 MB (63.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int recur(int ind1,int ind2,const string& s,const string& t,vector<vector<int>>& dp)
-    {
-        if(ind2<0) return 1;
-        if(ind1<0)
-        {
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        int dontTake=recur(ind1-1,ind2,s,t,dp);
-        int take=0;
-        if(s[ind1]==t[ind2])
-        {
-            take=recur(ind1-1,ind2-1,s,t,dp);
-        }
-        return dp[ind1][ind2]=take+dontTake;
-    }
+    // int recur(int ind1, int ind2, const string& s, const string& t,
+    //           vector<vector<int>>& dp) {
+    //     if (ind2 == 0)
+    //         return 1;
+    //     if (ind1 == 0) {
+    //         return 0;
+    //     }
+    //     if (dp[ind1][ind2] != -1)
+    //         return dp[ind1][ind2];
+    //     int dontTake = recur(ind1 - 1, ind2, s, t, dp);
+    //     int take = 0;
+    //     if (s[ind1 - 1] == t[ind2 - 1]) {
+    //         take = recur(ind1 - 1, ind2 - 1, s, t, dp);
+    //     }
+    //     return dp[ind1][ind2] = take + dontTake;
+    // }
     int numDistinct(string s, string t) {
-        int n1=s.size();
-        int n2=t.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return recur(n1-1,n2-1,s,t,dp);
+        int n1 = s.size();
+        int n2 = t.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int ind1 = 0; ind1 <= n1; ind1++) {
+            dp[ind1][0] = 1;
+        }
+        for (int ind1 = 1; ind1 <= n1; ind1++) {
+            for (int ind2 = 1; ind2 <= n2; ind2++) {
+                long long dontTake = dp[ind1 - 1][ind2];
+                long long take = 0;
+                if (s[ind1 - 1] == t[ind2 - 1]) {
+                    take = dp[ind1 - 1][ind2 - 1];
+                }
+                dp[ind1][ind2] = (int)take + dontTake;
+            }
+        }
+        return dp[n1][n2];
     }
 };
