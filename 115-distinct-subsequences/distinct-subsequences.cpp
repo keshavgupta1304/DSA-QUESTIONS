55cc047d912b31fff40afadd8057186dd9e0a198
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-09 22:35:14 +0530

    Time: 7 ms (96.85%) | Memory: 8.7 MB (97.86%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -19,20 +19,18 @@ public:
     int numDistinct(string s, string t) {
         int n1 = s.size();
         int n2 = t.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        for (int ind1 = 0; ind1 <= n1; ind1++) {
-            dp[ind1][0] = 1;
-        }
+        vector<int> cur(n2+1,0);
+        cur[0]=1;
         for (int ind1 = 1; ind1 <= n1; ind1++) {
-            for (int ind2 = 1; ind2 <= n2; ind2++) {
-                long long dontTake = dp[ind1 - 1][ind2];
+            for (int ind2 = n2; ind2 >= 1; ind2--) {
+                long long dontTake = cur[ind2];
                 long long take = 0;
                 if (s[ind1 - 1] == t[ind2 - 1]) {
-                    take = dp[ind1 - 1][ind2 - 1];
+                    take = cur[ind2 - 1];
                 }
-                dp[ind1][ind2] = (int)take + dontTake;
+                cur[ind2] = (int)take + dontTake;
             }
         }
-        return dp[n1][n2];
+        return cur[n2];
     }
 };
