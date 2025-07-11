5e405f68f8752b8c7f9516a5f46dfd7d573bb6f0
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-12 00:20:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (69.67%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int recur(int ind,int canbuy,const vector<int>& prices,vector<vector<int>>& dp)
-    {
-        if(ind==prices.size()) return 0;
-        if(dp[ind][canbuy]!=-1) return dp[ind][canbuy];
-        int profit=0;
-        if(canbuy)
-        {
-            profit=max(-prices[ind]+recur(ind+1,0,prices,dp),recur(ind+1,1,prices,dp));
-        }
-        else
-        {
-            profit=max(prices[ind]+recur(ind+1,1,prices,dp),recur(ind+1,0,prices,dp));
-        }
-        return dp[ind][canbuy]=profit;
-    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return recur(0,1,prices,dp);
+        int n = prices.size();
+        int aheadNotBuy = 0;
+        int aheadBuy = 0;
+        int curBuy, curNotBuy;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            curNotBuy = max(prices[ind] + aheadBuy, aheadNotBuy);
+            curBuy = max(-prices[ind] + aheadNotBuy, aheadBuy);
+            aheadBuy = curBuy;
+            aheadNotBuy = curNotBuy;
+        }
+        return aheadBuy;
     }
 };
