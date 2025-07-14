dfd41204bf8e1141a98e0875a405df00fe79b336
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-14 23:33:06 +0530

    Time: 67 ms (67.41%) | Memory: 79.5 MB (79.28%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     // int recur(int index, int canBuy, int maxTransactions,
     //           const vector<int>& prices, vector<vector<vector<int>>>& dp) {
-    //     if ((maxTransactions == 0 && canBuy == 1) || (index == prices.size()))
+    //     if ((maxTransactions == 0 && canBuy == 1) || (index ==
+    //     prices.size()))
     //         return 0;
     //     int profit = 0;
     //     if (dp[index][canBuy][maxTransactions] != -1)
@@ -14,39 +15,41 @@ public:
     //                 recur(index + 1, 1, maxTransactions, prices, dp));
     //     } else {
     //         profit = max(prices[index] +
-    //                          recur(index + 1, 1, maxTransactions, prices, dp),
+    //                          recur(index + 1, 1, maxTransactions, prices,
+    //                          dp),
     //                      recur(index + 1, 0, maxTransactions, prices, dp));
     //     }
     //     return dp[index][canBuy][maxTransactions] = profit;
     // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-
+        // vector<vector<vector<int>>> dp(
+        //     n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> next(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
         for (int index = n - 1; index >= 0; index--) {
             for (int canBuy = 0; canBuy <= 1; canBuy++) {
                 for (int maxTransactions = 0; maxTransactions <= 2;
                      maxTransactions++) {
                     int profit = 0;
                     if (canBuy == 1 && maxTransactions == 0)
-                        dp[index][canBuy][maxTransactions] = 0;
+                        curr[canBuy][maxTransactions] = 0;
                     else {
                         if (canBuy) {
-                            profit =
-                                max(-prices[index] + dp[index + 1][0]
-                                                           [maxTransactions - 1],
-                                    dp[index + 1][1][maxTransactions]);
+                            profit = max(-prices[index] +
+                                             next[0][maxTransactions - 1],
+                                         next[1][maxTransactions]);
                         } else {
-                            profit = max(prices[index] + dp[index + 1][1]
-                                                               [maxTransactions],
-                                         dp[index + 1][0][maxTransactions]);
+                            profit =
+                                max(prices[index] + next[1][maxTransactions],
+                                    next[0][maxTransactions]);
                         }
-                        dp[index][canBuy][maxTransactions] = profit;
+                        curr[canBuy][maxTransactions] = profit;
                     }
                 }
             }
+            next = curr;
         }
-        return dp[0][1][2];
+        return next[1][2];
     }
 };
