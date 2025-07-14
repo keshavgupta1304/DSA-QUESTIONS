01b6d291f409e674a7dc5d514108fdfa1d06d538
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-14 23:25:59 +0530

    Time: 418 ms (38.21%) | Memory: 211.6 MB (36.25%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    int recur(int index, int canBuy, int maxTransactions,
-              const vector<int>& prices,vector<vector<vector<int>>>& dp) {
-        if ((maxTransactions == 0 && canBuy == 1) || (index == prices.size()))
-            return 0;
-        int profit = 0;
-        if(dp[index][canBuy][maxTransactions]!=-1) return dp[index][canBuy][maxTransactions];
-        if (canBuy) {
-            profit = max(-prices[index] +
-                              recur(index + 1, 0, maxTransactions - 1, prices,dp),
-                          recur(index + 1, 1, maxTransactions, prices,dp));
-        } else {
-            profit = max(prices[index] +
-                              recur(index + 1, 1, maxTransactions, prices,dp),
-                          recur(index + 1, 0, maxTransactions, prices,dp));
-        }
-        return dp[index][canBuy][maxTransactions]=profit;
-    }
+    // int recur(int index, int canBuy, int maxTransactions,
+    //           const vector<int>& prices, vector<vector<vector<int>>>& dp) {
+    //     if ((maxTransactions == 0 && canBuy == 1) || (index == prices.size()))
+    //         return 0;
+    //     int profit = 0;
+    //     if (dp[index][canBuy][maxTransactions] != -1)
+    //         return dp[index][canBuy][maxTransactions];
+    //     if (canBuy) {
+    //         profit =
+    //             max(-prices[index] +
+    //                     recur(index + 1, 0, maxTransactions - 1, prices, dp),
+    //                 recur(index + 1, 1, maxTransactions, prices, dp));
+    //     } else {
+    //         profit = max(prices[index] +
+    //                          recur(index + 1, 1, maxTransactions, prices, dp),
+    //                      recur(index + 1, 0, maxTransactions, prices, dp));
+    //     }
+    //     return dp[index][canBuy][maxTransactions] = profit;
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return recur(0, 1, 2, prices,dp);
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                for (int maxTransactions = 0; maxTransactions <= 2;
+                     maxTransactions++) {
+                    int profit = 0;
+                    if (canBuy == 1 && maxTransactions == 0)
+                        dp[index][canBuy][maxTransactions] = 0;
+                    else {
+                        if (canBuy) {
+                            profit =
+                                max(-prices[index] + dp[index + 1][0]
+                                                           [maxTransactions - 1],
+                                    dp[index + 1][1][maxTransactions]);
+                        } else {
+                            profit = max(prices[index] + dp[index + 1][1]
+                                                               [maxTransactions],
+                                         dp[index + 1][0][maxTransactions]);
+                        }
+                        dp[index][canBuy][maxTransactions] = profit;
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
