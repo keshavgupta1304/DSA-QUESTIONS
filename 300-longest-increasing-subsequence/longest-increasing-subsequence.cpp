4946ed398072e17a062b276635fd98264af36018
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-16 18:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (99.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    // int recur(int ind, int prev_ind, const vector<int>& nums,
-    //           vector<vector<int>>& dp) {
-    //     if (ind == nums.size())
-    //         return 0;
-    //     if (dp[ind][prev_ind] != -1)
-    //         return dp[ind][prev_ind];
-    //     int len = recur(ind + 1, prev_ind, nums, dp);
-    //     if (prev_ind == 0 || nums[ind] > nums[prev_ind - 1]) {
-    //         len = max(len, 1 + recur(ind + 1, ind + 1, nums, dp));
-    //     }
-    //     return dp[ind][prev_ind] = len;
-    // }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        vector<int> curr(n+1,0);
-        vector<int> ahead(n+1,0);
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev_ind = n; prev_ind >= 0; prev_ind--) {
-                int len = ahead[prev_ind];
-                if (prev_ind == 0 || nums[ind] > nums[prev_ind - 1]) {
-                    len = max(len, 1 + ahead[ind + 1]);
-                }
-                curr[prev_ind] = len;
+        vector<int> temp;
+        int n=nums.size();
+        int len=1;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>temp.back())
+            {
+                temp.push_back(nums[i]);
+                len++;
             }
-            ahead=curr;
+            else
+            {
+                int index=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[index]=nums[i];
+            }
         }
-        return ahead[0];
+        return len;
     }
 };
