e64c3a193d0ad1318ebed9eac1c524f8c929a4c8
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-21 21:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.65%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int recur(int i,int n,int k,const vector<int>& arr,vector<int>& dp)
-    {
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int largestSum=0;
-        int maxi=0;
-        for(int index=i;index<k+i && index<n;index++)
-        {
-            maxi=max(maxi,arr[index]);
-            int currentSum=(index-i+1)*maxi + recur(index+1,n,k,arr,dp);
-            largestSum=max(largestSum,currentSum);
-        }
-        return dp[i]=largestSum;
-    }
+    // int recur(int i, int n, int k, const vector<int>& arr, vector<int>& dp) {
+    //     if (i == n)
+    //         return 0;
+    //     if (dp[i] != -1)
+    //         return dp[i];
+    //     int largestSum = 0;
+    //     int maxi = 0;
+    //     for (int index = i; index < k + i && index < n; index++) {
+    //         maxi = max(maxi, arr[index]);
+    //         int currentSum =
+    //             (index - i + 1) * maxi + recur(index + 1, n, k, arr, dp);
+    //         largestSum = max(largestSum, currentSum);
+    //     }
+    //     return dp[i] = largestSum;
+    // }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n=arr.size();
-        vector<int> dp(n,-1);
-        return recur(0,n,k,arr,dp);
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int largestSum = 0;
+            int maxi = 0;
+            for (int index = i; index < min(k+i,n); index++) {
+                maxi = max(maxi, arr[index]);
+                int currentSum =
+                    (index - i + 1) * maxi + dp[index + 1];
+                largestSum = max(largestSum, currentSum);
+            }
+            dp[i] = largestSum;
+        }
+        return dp[0];
     }
 };
