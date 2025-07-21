ee3a3af0d29651457d65ba8602a886f0021f8ab8
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-07-21 14:52:14 +0530

    Time: 106 ms (62.73%) | Memory: 14 MB (27.35%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    int recur(int i, int j, const vector<int>& nums, vector<vector<int>>& dp) {
-        if (i > j)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int maxCoins = INT_MIN;
-        for (int index = i; index <= j; index++) {
-            int coins = nums[i - 1] * nums[index] * nums[j + 1] +
-                        recur(i, index - 1, nums, dp) +
-                        recur(index + 1, j, nums, dp);
-            maxCoins = max(maxCoins, coins);
-        }
-        return dp[i][j] = maxCoins;
-    }
+    // int recur(int i, int j, const vector<int>& nums, vector<vector<int>>& dp) {
+    //     if (i > j)
+    //         return 0;
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int maxCoins = INT_MIN;
+    //     for (int index = i; index <= j; index++) {
+    //         int coins = nums[i - 1] * nums[index] * nums[j + 1] +
+    //                     recur(i, index - 1, nums, dp) +
+    //                     recur(index + 1, j, nums, dp);
+    //         maxCoins = max(maxCoins, coins);
+    //     }
+    //     return dp[i][j] = maxCoins;
+    // }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return recur(1, n, nums, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 1, 0));
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j <= n; j++) {
+                int maxCoins = INT_MIN;
+                for (int index = i; index <= j; index++) {
+                    int coins = nums[i - 1] * nums[index] * nums[j + 1] +
+                                dp[i][index - 1]+
+                                dp[index + 1][j];
+                    maxCoins = max(maxCoins, coins);
+                }
+                dp[i][j] = maxCoins;
+            }
+        }
+        return dp[1][n];
     }
 };
