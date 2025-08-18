c14c0992ad4ea8ad6c3243de0bfd53a88478391e
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-08-18 17:07:58 +0530

    Time: 83 ms (77.78%) | Memory: 292.7 MB (77.78%) - LeetSync

--- 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
+++ 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
@@ -1,27 +1,22 @@
 class Solution {
+    using ll=long long;
 public:
     long long minArraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<long long> dp(n+1,0);//max sum we can remove
-        map<int,long long> mp;
-        mp[0]=0LL; //j..i-1 tk ka array hoga jo remove ho skta hoga  dp[j]+(prefix[i]-prefix[j])->
-        //(dp[j] - prefix[j]) + prefix[i]
-        long long prefix=0;
-        for(int i=1;i<=n;i++)
+        vector<ll> best(k,-LLONG_MAX);best[0]=0;//max value of dp[j]-prefix[j] for every mod value
+        vector<ll> dp(n),prefix(n);//dp[i]=max sum removable from [0..i] prefix ->prefix Sum
+        for(int i=0;i<n;i++)
         {
-            prefix+=nums[i-1];
-            int rem=(int) (prefix%k);
-            dp[i]=dp[i-1];//skip
-            if(mp.count(rem))
+            prefix[i]+=nums[i];
+            if(i) prefix[i]+=prefix[i-1],dp[i]=dp[i-1];
+            if(best[prefix[i]%k]!=-LLONG_MAX)
             {
-                long long candidate=mp[rem]+prefix;
-                dp[i]=max(dp[i],candidate);
+                dp[i]=max(dp[i],prefix[i]+best[prefix[i]%k]);
             }
-            long long val=dp[i]-prefix;
-            mp[rem] = max(mp.count(rem) ? mp[rem] : LLONG_MIN, val);
+            best[prefix[i]%k]=max(best[prefix[i]%k],dp[i]-prefix[i]);
         }
-        long long totalSum=0;
-        for(auto &num:nums) totalSum+=num;
-        return totalSum-dp[n];
+        ll totalSum=0;
+        for(auto &x:nums) totalSum+=x;
+        return totalSum-dp[n-1];
     }
 };
