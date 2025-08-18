334b30b91c1adf6e545bd8d91117c31c19380ad7
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-08-19 01:39:21 +0530

    Time: 1209 ms (5.08%) | Memory: 541.8 MB (6.13%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -1,13 +1,64 @@
 class Solution {
+    using ll=long long;
 public:
+    ll m=1e9+7;
+
+    ll modmul(ll a,ll b)
+    {
+        return ((a%m)*(b%m))%m;
+    } 
+    ll modexp(ll a,ll exp)
+    {
+        ll res=1;
+        a%=m;
+        while(exp)
+        {
+            if(exp&1) res=modmul(res,a);
+            a=modmul(a,a);
+            exp=exp>>1;
+        }
+        return res;
+    }
+    ll modinv(ll a){
+        return modexp(a,m-2);
+    }
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        const int mod=1e9+7;
+        int n=nums.size();
+        int B=300;
+        vector<vector<int>> mul(B,vector<int>(n,1));
         for(auto &q:queries)
         {
             int l=q[0],r=q[1],k=q[2],v=q[3];
-            for(int idx=l;idx<=r;idx+=k)
+            if(k>=B)
             {
-                nums[idx]=((long long)nums[idx] * v) % (mod);
+                for(int i=l;i<=r;i+=k)
+                {
+                    nums[i]=modmul(nums[i],v);
+                }
+            }
+            else
+            {
+                mul[k][l]=modmul(mul[k][l],v);
+                for(int i=r+1;i<n;i++)
+                {
+                    if(i%k==l%k)
+                    {
+                        mul[k][i]=modmul(mul[k][i],modinv(v));
+                        break;
+                    }
+                }
+            }
+        }
+        for(int k=1;k<B;k++)
+        {
+            for(int st=0;st<k;st++)
+            {
+                int val=1;
+                for(int i=st;i<n;i+=k)
+                {
+                    val=modmul(val,mul[k][i]);
+                    nums[i]=modmul(nums[i],val);
+                }
             }
         }
         int ans=0;
