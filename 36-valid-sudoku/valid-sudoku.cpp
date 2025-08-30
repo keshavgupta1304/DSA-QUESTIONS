3f47e49cf5e9c9351f415cdb9defe326f8e3c612
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-08-30 22:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (96.21%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string> st;
-
-        for(int i=0;i<9;i++) {
-            for(int j=0;j<9;j++) {
-                if(board[i][j]=='.')
-                continue;
-                string row=string(1,board[i][j])+"_ROW_"+string(1,'0'+i);
-                string col=string(1,board[i][j])+"_COL_"+string(1,'0'+j);
-                string box=string(1,board[i][j])+"_BOX_"+string(1,'0'+(((i/3)*3)+(j/3)));
-
-                if(st.find(row)!=st.end() ||st.find(col)!=st.end() ||st.find(box)!=st.end()  )
-                return false;
-                st.insert(row);
-                st.insert(col);
-                st.insert(box);
+        return solver(board);
+    }
+    bool solver(vector<vector<char>>& board)
+    {
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.') continue;
+                if(!isPossible(i,j,board)) return false;
             }
         }
         return true;
     }
+    bool isPossible(int row,int col,vector<vector<char>>& board)
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(i!=col && board[row][i]==board[row][col])
+            {
+                return false;
+            }
+            if(i!=row && board[i][col]==board[row][col])
+            {
+                return false;
+            }
+            int r=3*(row/3)+i/3;
+            int c=3*(col/3)+i%3;
+            if(!(r==row && c==col) && board[r][c]==board[row][col]) return false;
+        }
+        return true;
+    }
+
 };
