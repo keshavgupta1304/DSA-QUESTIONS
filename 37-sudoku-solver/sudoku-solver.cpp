8dcab4c5c07a9936d645ff68793668cd8d4511e2
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2025-08-31 16:42:07 +0530

    Time: 372 ms (50.94%) | Memory: 8.7 MB (87.81%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& board, int row, int col,char num) {
-        for(int idx = 0; idx < 9; ++idx)
-            if(board[row][idx] == num || board[idx][col] == num)
-                return false;
-        int st_row = row / 3 * 3, st_col = col / 3 * 3;
-        for (int i = st_row; i < st_row + 3; i++) 
-            for (int j = st_col; j < st_col + 3; j++) 
-                if (board[i][j] == num) 
-                    return false;
-        return true;
+    void solveSudoku(vector<vector<char>>& board) {
+        solver(board);
     }
-
-    bool solveSudoku(vector<vector<char>>& board) {
-        for(int row = 0; row < 9; row++) {
-            for(int col= 0; col < 9; col++) {
-                if(board[row][col] == '.') {
-                    for(char num = '1'; num <= '9'; num++) {
-                        if (isValid(board, row, col, num)) {
-                            board[row][col] = num; 
-                            if (solveSudoku(board)) return true;
-                            board[row][col] = '.'; 
+    bool solver(vector<vector<char>>& board)
+    {
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(isPossible(i,j,c,board))
+                        {
+                            board[i][j]=c;
+                            if(solver(board)==true) return true;
+                            else board[i][j]='.';
                         }
                     }
                     return false;
                 }
             }
+        }
+        return true;
+    }
+    bool isPossible(int row,int col,char c,vector<vector<char>>& board)
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[row][i]==c)
+            {
+                return false;
+            }
+            if(board[i][col]==c)
+            {
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
         }
         return true;
     }
